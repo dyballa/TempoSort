d536431a6a08ce753a917ce2db0c83d30b304264
Luciano <dyballa@gmail.com>
2025-02-03 17:22:59 +0100

    add kilosort4 60 sec analysis

---
+++ data/2017_UCL_Neuropixels/60s/kilosort4/params.py
@@ -0,0 +1,6 @@
+n_channels_dat = 385
+offset = 0
+sample_rate = 30000
+dtype = 'int16'
+hp_filtered = False
+dat_path = '/gpfs/gibbs/project/zucker/ld562/spike-sort/kilo-tests/short/Hopkins_20160722_g0_t0.imec.ap_CAR.60sec.bin'
