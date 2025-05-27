71b8067b766fa47879566bfc413903dd0aa09f75
Lila Schweinfurth <lss72@r108u25n01.mccleary.ycrc.yale.edu>
2025-05-26 22:40:04 -0400

    added marques-smith 42 and 24

---
+++ data/2018_Marques-Smith/60s_42/params.py
@@ -0,0 +1,6 @@
+n_channels_dat = 384
+offset = 0
+sample_rate = 30000
+dtype = 'int16'
+hp_filtered = False
+dat_path = '/vast/palmer/scratch/krishnaswamy_smita/lss72/spike_sort/c42/c42_npx_raw.bin'
