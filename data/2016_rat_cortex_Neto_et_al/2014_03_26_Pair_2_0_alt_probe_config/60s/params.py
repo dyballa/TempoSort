a8221004d6d5334aaf3fb26321c1922b7d75ae82
Lila Schweinfurth <lss72@r108u25n01.mccleary.ycrc.yale.edu>
2025-06-16 22:22:18 -0400

    jim filter kilosort4 attempt

---
+++ data/2016_rat_cortex_Neto_et_al/2014_03_26_Pair_2_0_alt_probe_config/60s/params.py
@@ -0,0 +1,6 @@
+n_channels_dat = 32
+offset = 0
+sample_rate = 30000
+dtype = 'float32'
+hp_filtered = False
+dat_path = '/vast/palmer/scratch/krishnaswamy_smita/lss72/spike_sort/2014_03_26_Pair_2_0'
---
+++ data/2016_rat_cortex_Neto_et_al/2014_11_25_Pair_3_0_alt_probe_config/60s/params.py
@@ -0,0 +1,6 @@
+n_channels_dat = 32
+offset = 0
+sample_rate = 30000
+dtype = 'uint16'
+hp_filtered = False
+dat_path = '/vast/palmer/scratch/krishnaswamy_smita/lss72/spike_sort/2014_11_25_Pair_3_0'
